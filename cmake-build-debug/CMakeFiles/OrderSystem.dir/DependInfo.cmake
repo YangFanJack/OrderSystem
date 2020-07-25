4e175b1573db31ac1465ade702803b39cea0f070
yangfanjack <1144536063@qq.com>
2020-07-25 22:33:18 +0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/OrderSystem.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/Acode/C++/Clion/OrderSystem/Menu.cpp" "D:/Acode/C++/Clion/OrderSystem/cmake-build-debug/CMakeFiles/OrderSystem.dir/Menu.cpp.obj"
+  "D:/Acode/C++/Clion/OrderSystem/User.cpp" "D:/Acode/C++/Clion/OrderSystem/cmake-build-debug/CMakeFiles/OrderSystem.dir/User.cpp.obj"
+  "D:/Acode/C++/Clion/OrderSystem/main.cpp" "D:/Acode/C++/Clion/OrderSystem/cmake-build-debug/CMakeFiles/OrderSystem.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/OrderSystem.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OrderSystem.dir/Menu.cpp.obj"
+  "CMakeFiles/OrderSystem.dir/User.cpp.obj"
+  "CMakeFiles/OrderSystem.dir/main.cpp.obj"
+  "OrderSystem.exe"
+  "OrderSystem.exe.manifest"
+  "OrderSystem.pdb"
+  "libOrderSystem.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OrderSystem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
